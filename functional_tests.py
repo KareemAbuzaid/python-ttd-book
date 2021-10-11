26799036fe9c6d8a1287b22daab6f3e3ec540cd4
Kareem Abuzaid <kareem.abuzaid@muchconsulting.de>
2021-10-11 17:36:57 +0200

    test: use unittest for tests
    Signed-off-by: Kareem Abuzaid <kareem.abuzaid@muchconsulting.de>

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,23 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith hears about this great new to-do
+        # app, she checks out the homepage
+        self.browser.get('http://localhost:8000')
+        
+        # she notices the page title
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
 
