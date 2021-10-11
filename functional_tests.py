9db5511af06d87a8a67c5b76607846c1f4a1c695
Kareem Abuzaid <kareem.abuzaid@muchconsulting.de>
2021-10-11 20:12:45 +0200

    feat: add functional tests
    Signed-off-by: Kareem Abuzaid <kareem.abuzaid@muchconsulting.de>

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 import unittest
+import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -10,13 +12,32 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        
         # Edith hears about this great new to-do
         # app, she checks out the homepage
         self.browser.get('http://localhost:8000')
         
-        # she notices the page title
+        # she notices the page title and header
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # she is invited to add a todo item right away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # she types a to do
+        inputbox.send_keys('Buy peacock feathers')
+
+        # when she hits enter the page updates
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows  = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+
+        self.fail('Finish the tests!')
 
 if __name__ == '__main__':
     unittest.main()
