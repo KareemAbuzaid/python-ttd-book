39a9cb7b510f26b0540d2fd253294190526da89a
Kareem Abuzaid <kareem.abuzaid@muchconsulting.de>
2021-10-23 13:46:07 +0200

    test: add FT for list validation

---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,38 @@
+import os
+import time
+from unittest import skip
+from .base import FunctionalTest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
+
+
+class ItemValidationTest(FunctionalTest):
+
+    def test_cannot_add_empty_list_items(self):
+        
+        self.browser.get(self.live_server_url)
+        
+        # add an empty item
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text, "You can't have an empty list item"))
+
+        # add a non empty item
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # add an empty item again
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
+        # see an error message
+        self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text, "You can't have an empty list item"))
+        
+        # can add items after error
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
+
