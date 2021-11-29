1632002e9017564b19c58383b7b0acc17963cdde
Kareem <kareem.abuzaid@muchconsulting.de>
2021-11-29 02:21:54 +0200

    add wait decorator

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -8,6 +8,19 @@ from selenium.common.exceptions import WebDriverException
 
 MAX_WAIT = 10
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
+
 
 class FunctionalTest(StaticLiveServerTestCase):
 
@@ -41,23 +54,19 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
     
+    @wait
     def wait_for(self, fn):
-        start_time = time.time()
-        while True:
-            try:
-               return fn() 
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        return fn()
 
+    @wait
     def wait_to_be_logged_in(self, email):
-        self.wait_for(lambda: self.browser.find_element_by_link_text('Log out'))
+        self.browser.find_element_by_link_text('Log out')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
 
+    @wait
     def wait_to_be_logged_out(self, email):
-        self.wait_for(lambda: self.browser.find_element_by_name('email'))
+        self.browser.find_element_by_name('email') 
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
 
