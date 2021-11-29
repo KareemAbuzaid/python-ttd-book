44f5cbeda711f8db5f4feb5e7d9cb63e3bfea030
Kareem <kareem.abuzaid@muchconsulting.de>
2021-11-29 03:33:19 +0200

    use yahoo mail for FTs

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,20 +1,57 @@
+import os
+import re
+import poplib
+import time
 from django.core import mail
 from selenium.webdriver.common.keys import Keys
-import re
+
 
 from .base import FunctionalTest
 
-TEST_EMAIL = 'kareem.abuzaid123@gmail.com'
 SUBJECT = 'Your login link for Superlists'
 
 
 class LoginTest(FunctionalTest):
 
+    def wait_for_email(self, test_email, subject):
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+        
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('pop.mail.yahoo.com')
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ['YAHOO_PASSWORD'])
+            while time.time() - start < 60:
+                count, _ = inbox.stat()
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print('getting msg', i)
+                    _, lines, __ = inbox.retr(i)
+                    lines = [l.decode('utf-8') for l in lines]
+                    if f'Subject: {subject}' in lines:
+                        email_id = i
+                        body = '\n'.join(lines)
+                        return body
+                time.sleep(5)
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit()
+
     def test_can_get_email_link_to_login(self):
+
+        if self.staging_server:
+            test_email = 'kareemabuzaid@yahoo.com'
+        else:
+            test_email = 'edith@example.com'
 
         # enter email address
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+        self.browser.find_element_by_name('email').send_keys(test_email)
         self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
 
         # message stating that email has been sent
@@ -22,14 +59,14 @@ class LoginTest(FunctionalTest):
 
         # see email message
         email = mail.outbox[0]
-        self.assertIn(TEST_EMAIL, email.to)
+        self.assertIn(test_email, email.to)
         self.assertEqual(email.subject, SUBJECT)
 
         # the url link is in the mail
-        self.assertIn('Use this link to log in', email.body)
-        url_search = re.search(r'http://.+/.+$', email.body)
+        self.assertIn('Use this link to log in', body)
+        url_search = re.search(r'http://.+/.+$', body)
         if not url_search:
-            self.fail(f'Could not find url in email body:\n{email.body}')
+            self.fail(f'Could not find url in email body:\n{body}')
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
 
@@ -37,11 +74,11 @@ class LoginTest(FunctionalTest):
         self.browser.get(url)
 
         # user is logged in
-        self.wait_to_be_logged_in(email=TEST_EMAIL)
+        self.wait_to_be_logged_in(email=test_email)
 
         # user logs out
         self.browser.find_element_by_link_text('Log out').click()
 
         # logged out
-        self.wait_to_be_logged_out(email=TEST_EMAIL)
+        self.wait_to_be_logged_out(email=test_email)
 
